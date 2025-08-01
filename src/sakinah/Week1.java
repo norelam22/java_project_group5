1f2aa5e6daab9fe94068840772df075f5d63415f
sakinah <sakinahamiri1@gmail.com>
2025-08-01 11:44:10 -0700

    My first code

---
+++ src/sakinah/Week1.java
@@ -0,0 +1,13 @@
+package sakinah;
+
+public class Week1 {
+
+    public static void main(String[] args) {
+
+    }
+}
+/*  Java Question 01 - Odd or Even
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even")*/
