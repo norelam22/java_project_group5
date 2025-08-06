52f732d49c146ece74507672e51568fe6583db63
alenaponomareva <alenanet1991@gmail.com>
2025-08-05 21:12:40 -0400

    my first project

--- src/elena/FirstClass.java
+++ src/elena/FirstClass.java
@@ -2,6 +2,6 @@ package elena;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello, group 5");
+        System.out.println("Hello, group 5 ");
     }
 }
