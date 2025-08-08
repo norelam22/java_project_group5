2647883de3835c1c6903b31183c861213656e1e3
mustafasarac <7mustafasarac@gmail.com>
2025-08-08 16:50:00 -0400

    FINRA project created.

---
+++ src/saracMustafa/finra.java
@@ -0,0 +1,21 @@
+package saracMustafa;
+
+public class finra {
+        public static void main(String[] args) {
+            printFINRA();
+        }
+
+        public static void printFINRA() {
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0) {
+                    System.out.print("FIN ");
+                } else if (i % 5 == 0) {
+                    System.out.print("RA ");
+                } else if (i % 15 == 0) {
+                    System.out.print("FINRA ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
