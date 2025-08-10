598eac4092de2ecfcbc55b4edf495b29e6ea3222
markmobe <markmobe.23@gmail.com>
2025-08-10 16:33:04 -0500

    mubarek finra task solution

---
+++ src/mubarek/FinraTask.java
@@ -0,0 +1,23 @@
+package mubarek;
+
+public class FinraTask {
+
+    public static void main(String[] args) {
+
+        String print = "";
+
+        // Loop through numbers 1 to 30
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) { //If i divisible by both 3 and 5 print FINRA
+                print = "FINRA";
+            } else if (i % 3 == 0) {   //else If i divisible by 3 print FIN
+                print = "FIN";
+            } else if (i % 5 == 0) { //else If i divisible by 5 print RA
+                print = "RA";
+            } else {
+                System.out.print(i); // Else, not divisible by 3 or 5 → Print the number itself
+            }
+            System.out.println(print);
+        }
+    }
+}
