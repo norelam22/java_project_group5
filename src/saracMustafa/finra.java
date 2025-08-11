c95068cc63f0eb63ca896519e0d39f2283f29ea0
7mustafasarac <7mustafasarac@gmail.com>
2025-08-11 17:16:14 -0400

    Update finra.java

--- src/saracMustafa/finra.java
+++ src/saracMustafa/finra.java
@@ -7,12 +7,12 @@ public class finra {
 
         public static void printFINRA() {
             for (int i = 1; i <= 30; i++) {
-                if (i % 3 == 0) {
-                    System.out.print("FIN ");
+                if (i % 15 == 0) {
+                    System.out.print("FINRA ");
                 } else if (i % 5 == 0) {
                     System.out.print("RA ");
-                } else if (i % 15 == 0) {
-                    System.out.print("FINRA ");
+                } else if (i % 3 == 0) {
+                    System.out.print("FIN ");
                 } else {
                     System.out.print(i + " ");
                 }
