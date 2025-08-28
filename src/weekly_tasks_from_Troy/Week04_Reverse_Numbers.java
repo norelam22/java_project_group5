47a0cebe647ad0766450e47a26a79bceba042288
alenaponomareva <alenanet1991@gmail.com>
2025-08-27 21:19:40 -0400

    Reverse Numbers is done. Can you check it, please?

--- src/weekly_tasks_from_Troy/Week04_Reverse_Numbers.java
+++ src/weekly_tasks_from_Troy/Week04_Reverse_Numbers.java
@@ -6,6 +6,21 @@ public class Week04_Reverse_Numbers {
     Write a return method that can reverse
 negative number and return it as int
      */
+      public static int reverse(int number) {
+          int reversed = 0;
+          if (number < 0) {
+              while (number < 0) {
+                  int digit = number % 10;
+                  reversed = reversed * 10 + digit;
+                  number /= 10;
+              }
 
-    //Mehmet is volunteer
+          }
+          return reversed;
+      }
+
+    public static void main(String[] args) {
+        System.out.println("reversed= " + reverse(-3219));
+    }
 }
+
