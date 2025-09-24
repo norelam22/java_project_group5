68d96247605c9b31be63a3fbb279779f2c3d1ca8
TroyYC <troy@cydeo.com>
2025-09-24 13:02:34 -0400

    task 8 added

--- src/weekly_tasks_from_Troy/Week03_SwapTwoNumbers.java
+++ src/weekly_tasks_from_Troy/Week03_SwapTwoNumbers.java
@@ -1,11 +1,12 @@
 package weekly_tasks_from_Troy;
 
-public class SwapTwoNumbers {
+public class Week03_SwapTwoNumbers {
 
     /*
     Swap two variable values without using a third variable
      */
 
-    // karen is volunteer
+    // Karen is volunteer
+
 
 }
