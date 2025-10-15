5b98ade890085b786ae8db5e3c72d627770a95cf
TroyYC <troy@cydeo.com>
2025-10-15 12:41:20 -0400

    task 11 added

---
+++ src/weekly_tasks_from_Troy/Week11_SortAscending.java
@@ -0,0 +1,21 @@
+package weekly_tasks_from_Troy;
+
+public class Week11_SortAscending {
+
+    /*
+    Write a method that can sort an int array in Ascending order without using the sort method
+
+    input :  {1, 5, 8, 3, 2, 11, -1}
+    output : [-1, 1, 2, 3, 5, 8, 11]
+
+    Shaista is volunteer
+
+     */
+
+
+
+
+
+
+
+}
